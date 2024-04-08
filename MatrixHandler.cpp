b76a19ae8b5f67fe744457b3ef94e886bee1c573
Tishyn Oleksandr <158571708+9Oleksandr@users.noreply.github.com>
2024-04-08 22:10:26 +0200

    Update MatrixHandler.cpp
    Додані конструктор, деструктор класу та методи генерації, вводу з клавіатури та виводу матриці.

--- MatrixHandler.cpp
+++ MatrixHandler.cpp
@@ -1,11 +1,97 @@
-// MatrixHandler.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Matrix 
+{
+
+private:
+    int** data;
+    int rows;
+    int cols;
+
+public:
+    // Конструктор класу
+    Matrix(int rows, int cols) : rows(rows), cols(cols) {
+        data = new int* [rows];
+        for (int i = 0; i < rows; ++i) {
+            data[i] = new int[cols];
+            for (int j = 0; j < cols; ++j) {
+                data[i][j] = 0;
+            }
+        }
+    }
+    // Деструктор класу
+    ~Matrix()
+    {
+        for (int i = 0; i < rows; i++) 
+        {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    // Метод генерації матриці
+    void generateMatrix(int min, int max) 
+    {
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < cols; j++) 
+            {
+                data[i][j] = rand() % (max - min + 1) + min;
+            }
+        }
+    }
+
+    // Метод вводу матриці
+    void inputMatrix() 
+    {
+        cout << "Введiть елементи матрицi";
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < cols; j++) 
+            {
+                cout << "Введiть елемент у позицiю [" << i << "][" << j << "]: ";
+                cin >> data[i][j];
+            }
+        }
+    }
+    // Метод виводу матриці
+    void outputMatrix() 
+    {
+        cout << "Матриця:" << endl;
+        for (int i = 0; i < rows; i++) 
+        {
+            for(int j = 0; j < cols; j++) 
+            {
+                cout << data[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
+    setlocale(LC_ALL, "");
+    int min = 1;
+    int max = 10;
+    int rows;
+    int cols;
+    cout << "Введiть кiлькiсть строк: ";
+    cin >> rows;
+    cout << "Введiть кiлькiсть стовпцiв: ";
+    cin >> cols;
+
+    Matrix matrix(rows, cols);
+
+    matrix.generateMatrix(min, max);
+    matrix.outputMatrix();
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
